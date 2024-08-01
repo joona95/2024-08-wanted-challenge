e447f7431fb0c2cc33f37ca3bda9812e0d24c905
Joona Choi <juna052@naver.com>
2024-08-01 22:21:01 +0900

    fix: 회원 유효성 검증 메세지 변경

--- src/main/kotlin/org/project/portfolio/Member.kt
+++ src/main/kotlin/org/project/portfolio/Member.kt
@@ -11,10 +11,10 @@ class Member(
 ) {
 
     init {
-        Preconditions.checkArgument(isValidEmail(this.email), "이메일 형식이 올바르지 않습니다.")
+        Preconditions.checkArgument(isValidEmail(this.email), "이메일 형식이 올바르지 않습니다. (ex. ooo@oooo.ooo)")
         Preconditions.checkArgument(isValidPhoneNumber(this.phoneNumber), "휴대폰 번호 형식이 올바르지 않습니다. (ex. 000-0000-0000)")
-        Preconditions.checkArgument(isValidMemberId(this.memberId), "아이디는 대소문자 형식이어야 합니다.")
-        Preconditions.checkArgument(isValidMemberName(this.memberName), "이름은 한글 형식이어야 합니다.")
-        Preconditions.checkArgument(isValidPassword(this.password), "비밀번호는 대소문자와 숫자 5글자 이상, 특수문자 2글자 이상 포함하여야 합니다.")
+        Preconditions.checkArgument(isValidMemberId(this.memberId), "아이디 형식이 올바르지 않습니다. (1글자 이상 대소문자)")
+        Preconditions.checkArgument(isValidMemberName(this.memberName), "이름 형식이 올바르지 않습니다. (1글자 이상 한글)")
+        Preconditions.checkArgument(isValidPassword(this.password), "비밀번호 형식이 올바르지 않습니다. (대소문자와 숫자 5글자 이상, 특수문자 2글자 이상 포함)")
     }
 }
