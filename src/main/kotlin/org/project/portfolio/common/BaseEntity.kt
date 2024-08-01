7fc53ae21453991d73e6bbd2c05d07876d1e9b88
Joona Choi <juna052@naver.com>
2024-08-01 22:51:23 +0900

    feat: 기본 entity 클래스 추가
    - 생성일자 및 수정일자 정보 저장

---
+++ src/main/kotlin/org/project/portfolio/common/BaseEntity.kt
@@ -0,0 +1,22 @@
+package org.project.portfolio.common
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+abstract class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime = LocalDateTime.now()
+
+    @LastModifiedBy
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime = LocalDateTime.now()
+}
