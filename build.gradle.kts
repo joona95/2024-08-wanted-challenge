efb7e33da11d06077865e404dd11d6b2572d73b1
Joona Choi <juna052@naver.com>
2024-08-01 22:21:23 +0900

    chore: 테스트 코드 작성 위해 spock 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+    groovy
 }
 
 group = "org.project"
@@ -25,6 +26,9 @@ dependencies {
     implementation("com.google.guava:guava:32.1.3-jre")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testImplementation("org.apache.groovy:groovy:4.0.18")
+    testImplementation("org.spockframework:spock-core:2.4-M1-groovy-4.0")
+    testImplementation("org.spockframework:spock-spring:2.4-M1-groovy-4.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
